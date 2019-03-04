4240e67c7eeb21e3613e0d6e3127396dab87a493
Carl Fredrik Samson <cf@samson.no>
2019-03-05 00:05:13 +0100

    added readme

--- src/main.rs
+++ src/main.rs
@@ -282,7 +282,7 @@ fn trace(mut origin: Vec3, mut direction: Vec3) -> Vec3 {
 fn main() {
     let w = 960.0;
     let h = 540.0;
-    let samples_count = 1;
+    let samples_count = 2;
 
     let position = Vec3::new_abc(-22.0, 5.0, 25.0);
     let goal = !(Vec3::new_abc(-3.0, 4.0, 0.0) + position * Vec3::from(-1.0));
